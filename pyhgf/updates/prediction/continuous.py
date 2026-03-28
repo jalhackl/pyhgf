d3cf8c969c7e97e8cad5916338220a41e781fb61
jalhackl <josef@hackl.co.at>
2026-03-28 18:21:57 +0100

    forgetting/regret/memory between partially masked multi-input

--- pyhgf/updates/prediction/continuous.py
+++ pyhgf/updates/prediction/continuous.py
@@ -1,38 +1,50 @@
 # Author: Nicolas Legrand <nicolas.legrand@cas.au.dk>
 
 from functools import partial
+from typing import Dict
 
 import jax.numpy as jnp
 from jax import Array, jit
-
 from pyhgf.typing import Edges
+
+#
+from typing import Dict
+from typing import TYPE_CHECKING, List, Tuple
+from pyhgf.typing import Sequence
+
+import jax.numpy as jnp
+from jax import grad, jit
+from jax.lax import cond
+from jax.tree_util import Partial
+from jax import lax
+
 
 
 @partial(jit, static_argnames=("edges", "node_idx"))
 def predict_mean(
-    attributes: dict,
+    attributes: Dict,
     edges: Edges,
     node_idx: int,
 ) -> Array:
     r"""Compute the expected mean of a continuous state node.
 
     The expected mean at time :math:`k` for a state node :math:`a` with optional value
-    parent(s) :math:`b` is in [1]_ given by:
+    parent(s) :math:`b` is given by:
 
     .. math::
 
         \hat{\mu}_a^{(k)} = \lambda_a \mu_a^{(k-1)} + P_a^{(k)}
 
-    where :math:`P_a^{(k)}` is the drift rate (the total predicted drift of the expected
-    mean, which sums the tonic and - optionally - phasic drifts). The variable
+    where :math:`P_a^{(k)}` is the drift rate (the total predicted drift of the mean,
+    which sums the tonic and - optionally - phasic drifts). The variable
     :math:`lambda_a` represents the state's autoconnection strength, with
-    :math:`\lambda_a \in [0, 1]`. When :math:`\lambda_a = 1`, the node is performing a
-    Gaussian Random Walk using the value :math:`P_a^{(k)}` as total drift rate. When
+    :math:`\lambda_a \in [0, 1]`. When :math:`lambda_a = 1`, the node is performing a
+    Gaussian Random Walk using the value :math:` P_a^{(k)}` as total drift rate. When
     :math:`\lambda_a < 1`, the state will revert back to the total mean :math:`M_a`,
     which is given by:
 
     .. math::
-            M_a = \frac{\rho_a + f\left(\hat{\mu}_b^{(k)}\right)} {1-\lambda_a},
+            M_a = \frac{\rho_a + f\left(x_b^{(k)}\right)} {1-\lambda_a},
 
     If :math:`\lambda_a = 0`, the node is not influenced by its own mean anymore, but
     by the value received by the value parent.
@@ -87,11 +99,9 @@ def predict_mean(
             child_position = edges[value_parent_idx].value_children.index(node_idx)
             coupling_fn = edges[value_parent_idx].coupling_fn[child_position]
             if coupling_fn is None:
-                parent_value = attributes[value_parent_idx]["expected_mean"]
+                parent_value = attributes[value_parent_idx]["mean"]
             else:
-                parent_value = coupling_fn(
-                    attributes[value_parent_idx]["expected_mean"]
-                )
+                parent_value = coupling_fn(attributes[value_parent_idx]["mean"])
 
             driftrate += psi * parent_value
 
@@ -104,11 +114,10 @@ def predict_mean(
 
 
 @partial(jit, static_argnames=("edges", "node_idx"))
-def predict_precision(attributes: dict, edges: Edges, node_idx: int) -> Array:
+def predict_precision(attributes: Dict, edges: Edges, node_idx: int) -> Array:
     r"""Compute the expected precision of a continuous state node.
 
-    The expected precision at time :math:`k` for a state node :math:`a` is given by
-    [1]_:
+    The expected precision at time :math:`k` for a state node :math:`a` is given by:
 
     .. math::
 
@@ -199,22 +208,20 @@ def predict_precision(attributes: dict, edges: Edges, node_idx: int) -> Array:
 
 
 @partial(jit, static_argnames=("edges", "node_idx"))
-def continuous_node_prediction(
-    attributes: dict, node_idx: int, edges: Edges, **args
-) -> dict:
-    """Update the expected mean and expected precision of a continuous node [1]_.
+def continuous_node_prediction_standard(
+    attributes: Dict, node_idx: int, edges: Edges, **args
+) -> Dict:
+    """Update the expected mean and expected precision of a continuous node.
 
     Parameters
     ----------
     attributes :
         The attributes of the probabilistic nodes.
-
     .. note::
         The parameter structure also incorporates the value and volatility coupling
         strength with children and parents (i.e. `"value_coupling_parents"`,
         `"value_coupling_children"`, `"volatility_coupling_parents"`,
         `"volatility_coupling_children"`).
-
     node_idx :
         Pointer to the node that will be updated.
     edges :
@@ -254,6 +261,228 @@ def continuous_node_prediction(
     )
 
     # Update this node's parameters
+
+    # 1. input node without volatility parent
+    if (
+        (edges[node_idx].value_children is None)
+        and (edges[node_idx].volatility_children is None)
+        and (edges[node_idx].volatility_parents is None)
+    ):
+        attributes[node_idx]["expected_precision"] = attributes[node_idx]["precision"]
+
+    # 2. regular continuous state node, or input with volatility parent
+    else:
+        attributes[node_idx]["expected_precision"] = expected_precision
+
+    attributes[node_idx]["temp"]["effective_precision"] = effective_precision
+    attributes[node_idx]["expected_mean"] = expected_mean
+
+    return attributes
+
+
+
+
+# new: adding 'forget' functionality for multiple, partially masked inputs
+
+@partial(jit, static_argnames=("edges", "node_idx"))
+def continuous_node_prediction(
+    attributes: Dict, node_idx: int, edges: Edges, **args
+) -> Dict:
+    """Update the expected mean and expected precision of a continuous node.
+
+    Parameters
+    ----------
+    attributes :
+        The attributes of the probabilistic nodes.
+    .. note::
+        The parameter structure also incorporates the value and volatility coupling
+        strength with children and parents (i.e. `"value_coupling_parents"`,
+        `"value_coupling_children"`, `"volatility_coupling_parents"`,
+        `"volatility_coupling_children"`).
+    node_idx :
+        Pointer to the node that will be updated.
+    edges :
+        The edges of the probabilistic nodes as a tuple of
+        :py:class:`pyhgf.typing.Indexes`. The tuple has the same length as the node
+        number. For each node, the index lists the value and volatility parents and
+        children.
+
+    Returns
+    -------
+    attributes :
+        The updated attributes of the probabilistic nodes.
+
+    See Also
+    --------
+    update_continuous_input_parents, update_binary_input_parents
+
+    References
+    ----------
+    .. [1] Weber, L. A., Waade, P. T., Legrand, N., Møller, A. H., Stephan, K. E., &
+       Mathys, C. (2023). The generalized Hierarchical Gaussian Filter (Version 1).
+       arXiv. https://doi.org/10.48550/ARXIV.2305.10937
+
+    """
+
+    # if this node has volatility parent(s), store the current variance
+    # to be used by the posterior update if using unbounded approximation
+    attributes[node_idx]["temp"]["current_variance"] = (
+        1 / attributes[node_idx]["precision"]
+    )
+
+    # Get the new expected mean
+    expected_mean = predict_mean(attributes, edges, node_idx)
+
+    # Get the new expected precision and predicted volatility (Ω)
+    expected_precision, effective_precision = predict_precision(
+        attributes, edges, node_idx
+    )
+
+
+    #NEW - mode 4 is working fine, 0 (none) is default
+    mode_map = {"none": 0, "hgf_decay": 1, "regret_decay": 2, "regret_additive_precision": 3, "regret_additive_mult": 4}
+
+
+    default_observed_from  = {
+    2: 0,   # when updating node 2, check node 0
+    3: 1,   # when updating node 3, check node 1
+    }
+
+    default_regret_from    = {
+    2: 1,   # when updating node 2, check node 1
+    3: 0,   # when updating node 3, check node 0
+    }
+    
+    # Allow override from global config node (attributes[-1])
+    observed_from = attributes[-1].get("observed_from", default_observed_from)
+    regret_from   = attributes[-1].get("regret_from",   default_regret_from)
+    
+    # which nodes observed value to use
+    source_idx = observed_from.get(node_idx, node_idx)
+
+    # JAX: scalar boolean
+    observed = attributes[source_idx]["observed"]
+    cond_observed = (observed == 0)
+
+    # Extract mean_forget if it exists, otherwise 0 (JAX-friendly)
+    mean_forget = attributes[node_idx].get("mean_forget", 0.0)
+    mean_forgetneg = attributes[node_idx].get("mean_forgetneg", 0.0)
+    
+    #precision = attributes[node_idx]["precision"]
+    
+    
+
+    precision = attributes[node_idx]["precision"] - expected_precision
+    precision_val = attributes[node_idx]["precision"]
+    
+    
+    if "forget" in attributes[node_idx]:
+        time_multiplier = attributes[node_idx]["forget"]
+    else:
+        time_multiplier = 1
+        
+
+    # mean for differentiating between mean_forget and mean_forgetnet
+    source_mean = attributes[source_idx]["mean"]
+
+    source_regret_idx = regret_from.get(node_idx, node_idx)
+    source_regret_mean = attributes[source_regret_idx]["mean"]
+    # for the binary input, the 'mean' is either 1 (rewarded) or 0 (not rewarded)
+    
+    source_regret_precision = attributes[source_regret_idx]["precision"]
+
+    
+    if "precision_full" in attributes[-1]:
+        precision = precision_val
+    
+    precision_for_forgetting = precision
+    if "use_regret_precision" in attributes[-1]:
+        print("use_regret_precision")
+        precision_for_forgetting = source_regret_precision * attributes[-1]["use_regret_precision"] + precision * (1 - attributes[-1]["use_regret_precision"])
+
+    if "precision_full" in attributes[-1]:
+        precision_for_forgetting = precision_val
+
+    if "precision_other_weight" in attributes[node_idx]:
+        precision_other_weight = attributes[node_idx]["precision_other_weight"]
+    else:
+        precision_other_weight = 1
+    
+
+    if "use_effective_precision" in attributes[-1]:
+        print("use effective precision")
+        #precision_for_forgetting = source_regret_precision - precision_other_weight * source_regret_precision 
+        precision_for_forgetting = source_regret_precision / (1 + precision_other_weight * source_regret_precision)
+
+
+
+    def no_forgetting(pm):
+        
+        return pm
+
+
+    def hgf_decay(pm):
+        
+        return pm * jnp.exp(-mean_forget / precision_for_forgetting)
+
+
+    def regret_decay(pm):
+        
+        rate = (
+            mean_forget     * source_regret_mean
+            + mean_forgetneg * (1 - source_regret_mean)
+        )
+        return pm * jnp.exp(-rate / precision_for_forgetting)
+
+
+    def regret_additive_precision(pm):
+        
+        return (
+            pm
+            - (mean_forget     * source_regret_mean) / precision_for_forgetting
+            - (mean_forgetneg * (1 - source_regret_mean)) / precision_for_forgetting
+        )
+
+
+    def regret_additive_mult(pm):
+        
+        return (
+            pm
+            # in case of NO reward, this term is zero 
+            - mean_forget     * precision_for_forgetting * source_regret_mean
+            # in case of reward, this term is zero 
+            - mean_forgetneg * precision_for_forgetting * (1 - source_regret_mean)
+        )
+
+    mode_idx = attributes[-1].get("forget_mode", 0)
+    
+    def apply_forgetting(pm):
+        return lax.switch(
+            mode_idx,
+            [
+                no_forgetting,                # 0
+                hgf_decay,                    # 1
+                regret_decay,                 # 2
+                regret_additive_precision,    # 3
+                regret_additive_mult,         # 4
+            ],
+            pm,   
+        )
+
+
+    # lax.cond necessary instead of standard if-clause
+    def keep(pm):
+        return pm
+
+    expected_mean = lax.cond(
+        cond_observed,
+        apply_forgetting,
+        keep,
+        expected_mean,
+    )
+
+
+    # Update current node's parameters
 
     # 1. input node without volatility parent
     if (
